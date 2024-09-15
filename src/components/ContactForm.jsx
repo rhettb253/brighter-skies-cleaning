bc8081cca3c6c2d22f018853c71927462f637d59
Rhett Beardemphl <rhettbeardemphl@hotmail.com>
2024-09-15 08:41:50 -0700

    link server

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
+import axios from "axios";
 
 const ContactForm = () => {
+  let [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -11,10 +13,39 @@ const ContactForm = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  const sendEmail = 
+    async () => {
+      try {
+        setLoading(true);
+        // Make a POST request to your backend API
+        const response = await axios.post(
+          import.meta.env.VITE_SERVER_URL,
+          formData,
+          { headers: { "Content-Type": "application/json" } }
+        );
+        // Check the response and show success message
+        if (response.status === 200) { 
+          alert('Thank you for contacting us!');
+        }
+        // success message
+        setLoading(false);
+        console.log(response.data);
+      } 
+      catch (error) {
+        setLoading(false);
+        // show error
+        console.error("Error submitting the form", error);
+        alert(
+          `Error, ${error.message}.\nEmail not sent, please refresh and try again.`
+        );
+      }
+    };
+
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Handle the form submission, possibly using Nodemailer
-    alert('Thank you for contacting us!');
+    // Handle the form submission
+    sendEmail();
   };
 
   return (
@@ -33,7 +64,7 @@ const ContactForm = () => {
           Message:
           <textarea name="message" value={formData.message} onChange={handleChange} required />
         </label>
-        <button type="submit">Send</button>
+        {loading ? 'loading...' : <button type="submit">Send</button>}
       </form>
     </section>
   );
